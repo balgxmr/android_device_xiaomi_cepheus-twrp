53d7411039d768737e3821ffe06986da818b4138
pwnrazr <1644943+pwnrazr@users.noreply.github.com>
2023-03-13 17:53:23 +0800

    fbev2: fstab/twrp.flags: some changes
    Inspired/kanged from https://github.com/snnbyyds/twrp_device_oneplus_fajita/commit/94b3224f5e9ffc61f00e8c802429d6c79c93e84b
    
    * specify androidboot.boot_devices
     - Else we get errors with dynamic partitions kernel
    * build erofs utils
    * bring back some fstab and twrp.flags stuff
     - Allows backing up {system|vendor} though unsure how well it works since ofox already shows super under backup

--- device.mk
+++ device.mk
@@ -60,4 +60,7 @@ RECOVERY_LIBRARY_SOURCE_FILES += \
     $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/vendor.display.config@1.0.so \
     $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/vendor.display.config@2.0.so \
     $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/libdisplayconfig.qti.so \
- 
+
+# EROFS-utils
+PRODUCT_PACKAGES += \
+    erofs-utils
